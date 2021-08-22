81deec22cf5d930885dfb1efcff8a7b0d590cdb1
bel-sndct <79698612+bel-sndct@users.noreply.github.com>
2021-08-22 19:01:17 +0300

    feat: add min function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result = 0;
+    if (arguments.length && array.length !== 0) {
+        result = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < result) {
+                result = array[i];
+            }
+        }
+    }
+    return result;
 }
 
 exports.max = function max (array) {
